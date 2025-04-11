423dd40908d8b7a0d3c7e87620bd870421a04c11
Teahtimehh <kepocunwow@gmail.com>
2025-04-11 18:09:17 +0300

    Update number.py

--- number.py
+++ number.py
@@ -1,4 +1,16 @@
-number = int(input("Введите целое положительное число: "))
+def getpositiveinteger():
+    while True:
+        try:
+            number = int(input("Введите целое положительное число: "))
+            if number > 0:
+                return number
+            else:
+                print("Ошибка! Введено неправильное число. Попробуйте снова.")
+        except ValueError:
+            print("Ошибка! Вы ввели некорректное значение. Попробуйте снова.")
+
+
+number = getpositiveinteger()
 
 while number >= 0:
     print(number)
