6c4f864ac89cc2ae5a598aac70532af4d893ab03
Teahtimehh <kepocunwow@gmail.com>
2025-04-16 09:45:12 +0300

    Add files via upload

---
+++ my_list.py
@@ -0,0 +1,7 @@
+my_list = ["apple", "banana", "cherry", "watermelon", "lemon"]
+
+print("Исходный список:", my_list)
+
+my_list[0], my_list[-1] = my_list[-1], my_list[0]
+
+print("Изменённый список:", my_list)
